1ca1ba7e9a1acf6c9ad91258c7cdd303cfe29c6d
sergi.sanchez <sergi@dae-engineering.es>
2021-03-15 21:10:20 +0100

    About

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Language, MainRoute } from './constants/global';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,40 @@ import { Language, MainRoute } from './constants/global';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'monActiu';
+  // showLanguage: Indicates if language options must be shown
+  public showLanguage: boolean = false;
 
   // To use enums on template
   public MainRoute = MainRoute;
+  public Language = Language;
 
+  // language: current language
+  public language: Language = Language.CATALAN;
 
-  constructor (private _translate: TranslateService) {
-    this._translate.setDefaultLang (Language.CATALAN);
-    this._translate.use (Language.CATALAN);
+  // activeRoute: current route
+  public activeRoute: MainRoute = null;
+
+  constructor (private _translate: TranslateService,
+               private _router: Router) {
+    this._translate.setDefaultLang (this.language);
+    this._translate.use (this.language);
+
+    this._router.events.subscribe ((val: any) => {
+      if (val.hasOwnProperty ('url')) {
+        this.activeRoute = val.url.replace ('/', '');
+        // console.log ("[AppComponent] Route:", val.url);
+      }
+    });
   }
+
+  public toggleOptions () {
+    this.showLanguage = !this.showLanguage;
+  }
+
+  public selectLanguage (language: Language) {
+    this.showLanguage = false;
+    this.language = language;
+    this._translate.use (language);
+  }
+
 }
--- src/app/constants/global.ts
+++ src/app/constants/global.ts
@@ -3,21 +3,6 @@ export enum Language {
   CATALAN = 'ca-es',
 }
 
-export namespace Language {
-  export function keys () {
-    return Object.keys(Language).filter ((key: string) => key !== 'keys');
-  }
-}
-
-export function getLanguage (language: string) {
-  switch (language) {
-    case 'es-es':
-      return Language.SPANISH;
-    case 'ca-es':
-      return Language.CATALAN;
-  }
-}
-
 export enum MainRoute {
   HOME = '',
   ABOUT = 'about',
